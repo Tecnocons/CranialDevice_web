c5cfd77828320be4cb6f5612dd3395d37e416411
DavideGrieco <davide.g@tecnocons.com>
2024-07-22 13:19:16 +0200

    new:measurement table style

--- frontend/src/pages/patients/MeasurementsTable.jsx
+++ frontend/src/pages/patients/MeasurementsTable.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, IconButton, Typography, Select, MenuItem, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, IconButton, Typography, Select, MenuItem, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TablePagination } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import './MeasurementsTable.css';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const MeasurementsTable = ({ open, onClose, patientId }) => {
   const [measurements, setMeasurements] = useState([]);
@@ -8,6 +10,8 @@ const MeasurementsTable = ({ open, onClose, patientId }) => {
   const [selectedTimeframe, setSelectedTimeframe] = useState('today');
   const [groupedMeasurements, setGroupedMeasurements] = useState({});
   const [selectedMeasurementGroup, setSelectedMeasurementGroup] = useState(null);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   useEffect(() => {
     if (open) {
@@ -83,76 +87,99 @@ const MeasurementsTable = ({ open, onClose, patientId }) => {
     setSelectedMeasurementGroup(null);
   };
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+    <Dialog open={open} onClose={onClose} maxWidth="lg" fullWidth>
       <DialogTitle>
         Dettagli Misurazioni
         <IconButton onClick={onClose} style={{ position: 'absolute', right: 8, top: 8 }}>
           <CloseIcon />
         </IconButton>
       </DialogTitle>
-      <DialogContent>
+      <DialogContent className="measurements-dialog">
         {selectedMeasurementGroup ? (
           <>
-            <Button onClick={handleBackToGroups} style={{ marginBottom: '16px' }}>
-              INDIETRO
-            </Button>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Timestamp</TableCell>
-                    <TableCell>Spostamento (mm)</TableCell>
-                    <TableCell>Forza (N)</TableCell>
-                    <TableCell>Pressione (bar)</TableCell>
-                    <TableCell>Contropressione (bar)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {selectedMeasurementGroup.map((measurement, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{new Date(measurement.timestamp).toLocaleString()}</TableCell>
-                      <TableCell>{measurement.spostamento_mm}</TableCell>
-                      <TableCell>{measurement.forza_n}</TableCell>
-                      <TableCell>{measurement.pressione_bar}</TableCell>
-                      <TableCell>{measurement.contropressione_bar}</TableCell>
+            <div className="measurements-back-button" onClick={handleBackToGroups}>
+              <ArrowBackIcon className="measurements-back-icon" />
+              <Typography variant="button">INDIETRO</Typography>
+            </div>
+            <div className="measurements-table-wrapper">
+              <TableContainer component={Paper}>
+                <Table className="measurements-table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Timestamp</TableCell>
+                      <TableCell>Spostamento (mm)</TableCell>
+                      <TableCell>Forza (N)</TableCell>
+                      <TableCell>Pressione (bar)</TableCell>
+                      <TableCell>Contropressione (bar)</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+                  </TableHead>
+                  <TableBody>
+                    {selectedMeasurementGroup.map((measurement, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{new Date(measurement.timestamp).toLocaleString()}</TableCell>
+                        <TableCell>{measurement.spostamento_mm}</TableCell>
+                        <TableCell>{measurement.forza_n}</TableCell>
+                        <TableCell>{measurement.pressione_bar}</TableCell>
+                        <TableCell>{measurement.contropressione_bar}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
           </>
         ) : (
           <>
-            <Typography variant="h6">Misurazioni</Typography>
-            <Select
-              value={selectedTimeframe}
-              onChange={handleTimeframeChange}
-              style={{ marginBottom: '16px' }}
-            >
-              <MenuItem value="today">Oggi</MenuItem>
-              <MenuItem value="thisWeek">Questa settimana</MenuItem>
-              <MenuItem value="thisMonth">Questo mese</MenuItem>
-              <MenuItem value="thisYear">Quest'anno</MenuItem>
-            </Select>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Measurement ID</TableCell>
-                    <TableCell>Timestamp</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {Object.keys(groupedMeasurements).map(measurementId => (
-                    <TableRow key={measurementId} onClick={() => handleMeasurementClick(measurementId)} style={{ cursor: 'pointer' }}>
-                      <TableCell>{measurementId}</TableCell>
-                      <TableCell>{new Date(groupedMeasurements[measurementId][0].timestamp).toLocaleString()}</TableCell>
+            <div className="measurements-table-container">
+              <TableContainer component={Paper}>
+                <Table className="measurements-table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Timestamp</TableCell>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+                  </TableHead>
+                  <TableBody>
+                    {filteredMeasurements.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((measurement, index) => (
+                      <TableRow key={index} onClick={() => handleMeasurementClick(measurement.measurement_id)} style={{ cursor: 'pointer' }}>
+                        <TableCell>{new Date(measurement.timestamp).toLocaleString()}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component="div"
+                count={filteredMeasurements.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </div>
+            <div className="measurements-filter-container">
+              <Typography variant="h6">Misurazioni</Typography>
+              <Select
+                value={selectedTimeframe}
+                onChange={handleTimeframeChange}
+                style={{ marginBottom: '16px' }}
+              >
+                <MenuItem value="today">Oggi</MenuItem>
+                <MenuItem value="thisWeek">Questa settimana</MenuItem>
+                <MenuItem value="thisMonth">Questo mese</MenuItem>
+                <MenuItem value="thisYear">Quest'anno</MenuItem>
+              </Select>
+            </div>
           </>
         )}
       </DialogContent>
